64a295c32e1511648d4c3a0d8a6e34d61540aa96
binarycrossentropy <iqbalamo93@gmail.com>
2025-08-23 00:11:24 +0100

    using importlib

--- src/ssm_parameter_store/version.py
+++ src/ssm_parameter_store/version.py
@@ -1,7 +1,7 @@
 from pkg_resources import get_distribution, DistributionNotFound
-
+from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = get_distribution('ssm-parameter-store').version
-except DistributionNotFound:
+    __version__ = version("ssm-parameter-store")
+except PackageNotFoundError:
     __version__ = 'unknown'  # package not installed
